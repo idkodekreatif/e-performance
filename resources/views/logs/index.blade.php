5ad2675494bc23f08c4adac2d1046ca59321b733
Agus Baha <id.stackdevelopers@gmail.com>
2022-12-13 15:35:54 +0700

    rename table view logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -10,7 +10,7 @@
 
     <div class="card">
         <div class="card-header">
-            <h4 class="card-title">Responsive Table</h4>
+            <h4 class="card-title">Logs Activity</h4>
         </div>
         <div class="card-body">
             <div class="table-responsive">
