644051beb5bf78232b43bcb480b592d10f3fb0a9
AgusBaha <id.stackdevelopers@gmail.com>
2023-06-19 13:12:36 +0700

    refisi tanda tangan

--- resources/views/iktisar/lpm/iktisarBulananKaUnit/raportPdf.blade.php
+++ resources/views/iktisar/lpm/iktisarBulananKaUnit/raportPdf.blade.php
@@ -8,64 +8,67 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <style>
-        body {
-            margin-top: 13%;
-            font-family: Arial, Helvetica, sans-serif;
-        }
+        <style>
+            body {
+                margin-top: 13%;
+                font-family: Arial, Helvetica, sans-serif;
+            }
 
-        table,
-        th,
-        td {
-            width: 100%;
-            border: 1px solid #000000;
-            text-align: center;
-            font-size: 12px;
-            border-collapse: collapse;
-            padding: 5px;
-        }
+            table,
+            th,
+            td {
+                width: 100%;
+                border: 1px solid #000000;
+                text-align: center;
+                font-size: 12px;
+                border-collapse: collapse;
+                padding: 5px;
+            }
 
-        .tanda-tangan {
-            justify-content: space-between;
-            align-items: center;
-            margin-top: 5px;
-        }
+            .tanda-tangan {
+                justify-content: space-between;
+                align-items: center;
+                margin-top: 5px;
+            }
 
-        .pihak {
-            text-align: center;
-        }
+            .pihak1 {
+                text-align: center;
+            }
+            .pihak2 {
+                margin-top: 20%;
+                text-align: center;
+            }
 
-        .kiri {
-            margin-right: auto;
-            float: left;
-        }
+            .kiri {
+                margin-right: auto;
+                float: left;
+            }
 
-        .kanan {
-            margin-left: auto;
-            float: right;
-        }
+            .kanan {
+                margin-left: auto;
+                float: right;
+            }
 
-        .nama-dosen,
-        .nama-warek {
-            font-size: 14px;
-            font-weight: bold;
-        }
+            .nama-dosen,
+            .nama-warek {
+                font-size: 14px;
+                font-weight: bold;
+            }
 
-        .signatures {
-            /* display: flex; */
-            justify-content: space-between;
-            align-items: center;
-            text-align: center;
-        }
+            .signatures {
+                justify-content: space-between;
+                align-items: center;
+                text-align: center;
+            }
 
-        .center-signature {
-            margin-top: 20%;
-            width: 100%;
-            font-size: 14px;
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-    </style>
+            .center-signature {
+                margin-top: 20%;
+                width: 100%;
+                font-size: 14px;
+                font-weight: bold;
+                margin-bottom: 5px;
+            }
+        </style>
 </head>
 
 <body>
@@ -124,22 +127,34 @@
 
     <p style="text-align: right; margin-bottom:0;">Surabaya, {{ date('d F Y') }}</p>
 
-    <div class="tanda-tangan">
+    <div class="pihak1">
         <div class="pihak kiri">
-            <p class="nama-dosen">Penilai,</p>
+            <p class="nama-dosen">Pejabat Penilai,</p>
             <br>
-            <br>
-            <p class="nama-dosen">Setiya Hartiningtiyaswati, M. Keb</p>
+            <p class="nama-dosen">Setiya Hartiningtiyaswati, S.ST., M.Keb.</p>
         </div>
         <div class="pihak kanan">
-            <p class="nama-warek">Yang dinilai,</p>
+            <p class="nama-warek">Personel yang dinilai,</p>
             <br>
-            <br>
-            <p class="nama-warek">{{$data->name}}</p>
+            <p class="nama-warek">{{ $data->name }}</p>
         </div>
     </div>
 
-    <div class="signatures">
+    <div class="pihak2">
+        <div class="pihak kiri">
+            <p class="nama-dosen">Atasan Pejabat Penilai,</p>
+            <br>
+            <br>
+            <p class="nama-dosen">Annas Vijaya, S.Si., M.T.I.</p>
+        </div>
+        <div class="pihak kanan">
+            <p class="nama-warek">Pejabat yang <br> Menyetujui/Mengesahkan,</p>
+            <br>
+            <p class="nama-warek">Dr. Ahmad Hariyanto, M.Si.</p>
+        </div>
+    </div>
+
+    {{-- <div class="signatures">
         <div class="center-signature">
             <p>Menyetujui/Mengesahkan,</p>
             <p>Rektor,</p>
@@ -147,7 +162,7 @@
             <br>
             <p>Dr. Ahmad Hariyanto, M.Si.</p>
         </div>
-    </div>
+    </div> --}}
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
